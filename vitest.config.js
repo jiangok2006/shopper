23083163ff47cca0152b008e58e5dec95d7a44c1
Lian Jiang <lian.jiang@opendoor.com>
2023-11-30 12:26:50 -0800

    d

--- vitest.config.js
+++ vitest.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vitest';
+import { resolve } from 'node:path';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     coverage: {
       reporter: ['text', 'json-summary', 'json'],
     }
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: resolve(__dirname, ".") }]
   }
-});
+})
+
